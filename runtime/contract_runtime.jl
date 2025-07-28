ad314d3d54863361959bd3a859f7dc1ef319d607
xzgao <xz.gao@connect.ust.hk>
2025-07-29 01:07:53 +0800

    update

--- runtime/contract_runtime.jl
+++ runtime/contract_runtime.jl
@@ -43,13 +43,14 @@ end
 
 function contract_runtime()
     n = 70
-    ids = [3, 2, 4, 5, 7, 9, 10]
+    # ids = [3, 2, 4, 5, 7, 9, 10]
+    ids = [7, 9, 10]
     dir = @__DIR__
 
     graphs = loadgraphs(joinpath(dir, "../graphs/random_ksg/kernelized_tn_ksg_n$(n).dot"))
 
     df = joinpath(dir, "../data/runtime/contract_runtime_n$(n).csv")
-    CSV.write(df, DataFrame(name = String[], tnbb_runtime = Float64[], ds_runtime = Float64[]))
+    # CSV.write(df, DataFrame(name = String[], tnbb_runtime = Float64[], ds_runtime = Float64[]))
 
     for id in ids
         dir = "/Einstein/xzgao/tnbb_jlds/random_ksg/$(n)_$(id)_sc31/"
