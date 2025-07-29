e37723b6a40121df13e24b130c2dd25b469a15de
xzgao <xz.gao@connect.ust.hk>
2025-07-29 18:00:20 +0800

    update order

---
+++ runtime/ip_runtime.jl
@@ -0,0 +1,34 @@
+using Graphs, GraphIO
+using JuMP, SCIP
+using CSV, DataFrames
+
+function ip_mis(g::AbstractGraph; optimizer=SCIP.Optimizer, verbose::Bool=false)
+    model = Model(optimizer)
+    !verbose && set_silent(model)
+    n = nv(g)
+    @variable(model, 0 <= x[i = 1:n] <= 1, Int)
+    @objective(model, Max, sum(x))
+    for e in edges(g)
+        @constraint(model, x[src(e)] + x[dst(e)] <= 1)
+    end
+    optimize!(model)
+    @assert is_solved_and_feasible(model)
+    return Int(round(sum(value.(x))))
+end
+
+function scip_runtime(n)
+    dir = @__DIR__
+    df = joinpath(dir, "../data/runtime/ksg_n70_scip.csv") # try the kernelized graphs first
+    CSV.write(df, DataFrame(name = String[], runtime = Float64[]))
+
+    graphs = loadgraphs(joinpath(dir, "../graphs/random_ksg/kernelized_tn_ksg_n$(n).dot"))
+
+    for i in 1:10
+        g = graphs["$i"]
+        t = @elapsed ip_mis(g, verbose = true)
+        @show i, t
+        CSV.write(df, DataFrame(name = i, runtime = t), append = true)
+    end
+end
+
+scip_runtime(70)
--- runtime/slice_runtime.jl
+++ runtime/slice_runtime.jl
@@ -28,32 +28,30 @@ function slice_different_target(n, i)
     end
 end
 
-function slice_runtime(n)
+function slice_runtime_sc31(n, i)
     dir = @__DIR__
-    df = "data/random_ksg/complexity/ds_sc31_runtime_n$(n).csv"
-    CSV.write(df, DataFrame(name = String[], original_sc = Int[], total_tc = Int[], nslice = Int[], runtime = Float64[]))
+    df = joinpath(dir, "../data/runtime/ksg_n70_sc31_ds_branch.csv")
+    # CSV.write(df, DataFrame(name = String[], original_sc = Int[], total_tc = Int[], nslice = Int[], runtime = Float64[]))
 
-    for i in 1:10
-        code = readjson(joinpath(dir, "../graphs/random_ksg/order/treesa_tn_ksg_n$(n)_s$(i)_treesa.json"))
-        original_cc = mis_complexity(code)
+    code = readjson(joinpath(dir, "../graphs/random_ksg/order/treesa_tn_ksg_n$(n)_s$(i)_treesa.json"))
+    original_cc = mis_complexity(code)
 
-        original_cc.sc <= 31 && continue
-        slicer = TreeSASlicer(sc_target = 31)
+    original_cc.sc <= 31 && return nothing
+    slicer = TreeSASlicer(sc_target = 31)
 
-        sliced_code = slice_code(code, uniformsize(code, 2), slicer)
-        t = @elapsed slice_code(code, uniformsize(code, 2), slicer)
+    sliced_code = slice_code(code, uniformsize(code, 2), slicer)
+    t = @elapsed slice_code(code, uniformsize(code, 2), slicer)
 
-        sliced_cc = mis_complexity(sliced_code)
-        @show i, original_cc.sc, sliced_cc.sc, sliced_cc.tc, sliced_cc.rwc, t
-        @show sliced_cc.tc / sliced_cc.rwc
+    sliced_cc = mis_complexity(sliced_code)
+    @show i, original_cc.sc, sliced_cc.sc, sliced_cc.tc, sliced_cc.rwc, t
+    @show sliced_cc.tc / sliced_cc.rwc
 
-        writejson(joinpath(dir, "../graphs/random_ksg/order/slice31_tn_ksg_n$(n)_s$(i)_treesa.json"), sliced_code)
+    writejson(joinpath(dir, "../graphs/random_ksg/order/slice31_tn_ksg_n$(n)_s$(i)_treesa.json"), sliced_code)
 
-        CSV.write(df, DataFrame(name = i, original_sc = original_cc.sc, total_tc = sliced_cc.tc, nslice = length(sliced_code.slicing), runtime = t), append = true)
-    end
+    CSV.write(df, DataFrame(name = i, original_sc = original_cc.sc, total_tc = sliced_cc.tc, nslice = length(sliced_code.slicing), runtime = t), append = true)
 
     return nothing
 end
 
-# slice_runtime(70)
-slice_different_target(70, 7)
+slice_runtime_sc31(70, 5)
+# slice_different_target(70, 7)
