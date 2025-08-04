1051a6f77cfee2a0cfae4e5d26f57dc7a817f0a2
xzgao <xz.gao@connect.ust.hk>
2025-08-04 16:30:40 +0800

    update figs

--- drawer/rksg_compare_original.jl
+++ drawer/rksg_compare_original.jl
@@ -1,4 +1,5 @@
 include("settings.jl")
+t = markersize
 
 begin
     fig = Figure(backgroundcolor = RGBf(1.0, 1.0, 1.0), size = (500, 700), fontsize = 20)
@@ -36,7 +37,6 @@ begin
         end
     end
 
-    t = markersize
     sc_tn = scatter!(ax1, sc_tn, tc_tn, markersize = t, marker = markerstyle[2], color = colors[2], strokewidth = strokewidth, strokecolor = :black, label = "Tropical TN")
     sc_tnbb = scatter!(ax1, sc_tnbb, tc_tnbb, markersize = t, marker = markerstyle[3], color = colors[3], strokewidth = strokewidth, strokecolor = :black, label = "TNBB")
     sc_ds = scatter!(ax1, sc_ds, tc_ds, markersize = t, marker = markerstyle[4], color = colors[4], strokewidth = strokewidth, strokecolor = :black, label = "Dynamic Slicing")
@@ -92,9 +92,29 @@ begin
     fig = Figure(backgroundcolor = RGBf(1.0, 1.0, 1.0), size = (500, 700), fontsize = 20)
 
     ax1 = Axis(fig[3, 1], xlabel = L"N", ylabel = L"\text{log}_2(\text{tc})")
-    ax2 = Axis(fig[2, 1], xlabel = L"\text{sc}", ylabel = L"\text{log}_2(\text{tc})")
+    ax2 = Axis(fig[2, 1], xlabel = L"\text{sc}", ylabel = L"\text{log}_2(\text{tc})", xreversed = true, xticks = [33, 31, 29, 27, 25, 23, 21, 19, 17])
 
     text!(ax2, 0, 1, text = "(a)", align = (:left, :top), fontsize = 25, font = :bold, space = :relative, offset = (4, -2))
+
+    df_tnbb_vsc = CSV.read("../data/complexity/random_ksg/tnbb_different_target_freoptimize_n80.csv", DataFrame)
+    df_ds_vsc = CSV.read("../data/complexity/random_ksg/treesa_different_target_n80.csv", DataFrame)
+
+
+    i = 4
+    sc_tnbb = df_tnbb_vsc.target
+    tc_tnbb = df_tnbb_vsc.total_tc
+    orignal_tc = df_ds_vsc[df_ds_vsc.name .== i, :origin_tc][1]
+    orignal_sc = df_ds_vsc[df_ds_vsc.name .== i, :origin_sc][1]
+    sc_ds = df_ds_vsc[df_ds_vsc.name .== i, :sliced_sc][1:2:end]
+    tc_ds = df_ds_vsc[df_ds_vsc.name .== i, :sliced_tc][1:2:end]
+
+    scatter!(ax2, sc_ds, tc_ds, markersize = t, marker = markerstyle[4], color = colors[4], strokewidth = strokewidth, strokecolor = :black)
+    scatter!(ax2, sc_tnbb, tc_tnbb, markersize = t, marker = markerstyle[3], color = colors[3], strokewidth = strokewidth, strokecolor = :black)
+    scatter!(ax2, orignal_sc, orignal_tc, markersize = t, marker = markerstyle[2], color = :white, strokewidth = 2, strokecolor = colors[2])
+    hlines!(ax2, [orignal_tc], color = :blue, linestyle = :dash)
+    
+    xlims!(ax2, 34, 18)
+    ylims!(ax2, 39, 81)
 
     ## ax1, compare tropical tensor network and pure branch&bound
     n_tn = [50:10:100...]
@@ -109,12 +129,12 @@ begin
     tc_ds = [geometric_mean(df_ds[i].sliced_tc) for i in 1:length(n_ds)]
     tc_tnbb = [geometric_mean(df_tnbb[i].total_tc) for i in 1:length(n_tnbb)]
 
-    @. model_tn(x, p) = p[1] * x^p[2] + p[3]
-    fit_tn = curve_fit(model_tn, n_tn, tc_tn, [1.0, 1.0, 1.0])
+    @. model_tn(x, p) = p[1] * x + p[2]
+    fit_tn = curve_fit(model_tn, n_tn, tc_tn, [1.0, 1.0])
     @. model_ds(x, p) = p[1] * x^p[2] + p[3]
     fit_ds = curve_fit(model_ds, n_ds, tc_ds, [1.0, 1.0, 1.0])
-    @. model_tnbb(x, p) = p[1] * x^p[2] + p[3]
-    fit_tnbb = curve_fit(model_tnbb, n_tnbb, tc_tnbb, [1.0, 1.0, 1.0])
+    @. model_tnbb(x, p) = p[1] * x + p[2]
+    fit_tnbb = curve_fit(model_tnbb, n_tnbb, tc_tnbb, [1.0, 1.0])
 
 
     xs = range(45, 105, length = 100)
@@ -130,7 +150,7 @@ begin
     xlims!(ax1, 45, 105)
     ylims!(ax1, 20, 100)
 
-    Legend(fig[1, :], [sc_tn, sc_ds, sc_tnbb], ["Tropical TN", "Dynamic Slicing", "TNBB"], orientation = :horizontal, nbanks = 1, labelsize = 12)
+    Legend(fig[1, :], [sc_tn, sc_ds, sc_tnbb], ["Tropical TN", "Dynamic Slicing", "BBTN"], orientation = :horizontal, nbanks = 1, labelsize = 12)
     text!(ax1, 0, 1, text = "(b)", align = (:left, :top), fontsize = 25, font = :bold, space = :relative, offset = (4, -2))
 
     
@@ -142,6 +162,7 @@ begin
     hlines!(ax1, [tc_100_years], color = :black, linestyle = hstyle, linewidth = hwidth)
     text!(ax1, 46, tc_100_years, text = L" \textbf{100 years}", color = :black, fontsize = 16)
 
+    save("../figs/tnbb_random_ksg.pdf", fig)
 
     fig
 end
--- tnbb/tnbb_krksg_n80_vsc.jl
+++ tnbb/tnbb_krksg_n80_vsc.jl
@@ -12,30 +12,29 @@ function tnbb_krksg_different_target(n)
 
     data = CSV.read(joinpath(dir, "../data/kernelize/tn_ksg_n$(n).csv"), DataFrame)
 
-    df = "data/complexity/tnbb_different_target_n$(n).csv"
-    # CSV.write(df, DataFrame(name = String[], ds = Int[], total_tc = Int[], num_branch = Int[]))
+    df = "data/complexity/random_ksg/tnbb_different_target_freoptimize_n$(n).csv"
+    # CSV.write(df, DataFrame(name = String[], target = Int[], total_tc = Int[], num_branch = Int[]))
 
-    for i in 1:2
+    for i in [4]
         g = graphs["$i"]
         r = data[data.name .== i, :r][1]
         @show i, nv(g), ne(g), r
         code = readjson(joinpath(dir, "../graphs/random_ksg/order/treesa_tn_ksg_n$(n)_s$(i)_treesa.json"))
         original_cc = mis_complexity(code)
-        for ds in 11:15
-            target = original_cc.sc - ds
-            slicer = ContractionTreeSlicer(sc_target = target)
+        for target in 24:-2:16
+            slicer = ContractionTreeSlicer(sc_target = target, refiner = TreeSARefiner(reoptimize = false))
             # reducer = TensorNetworkReducer(sub_reducer = XiaoReducer())
             reducer = XiaoReducer()
             branch = SlicedBranch(g, UnitWeight(nv(g)), code, r)
-            dirname = "/Einstein/xzgao/tnbb_jlds/random_ksg/$(n)_$(i)_ds$(ds)/"
+            dirname = "/Einstein/xzgao/tnbb_jlds/random_ksg/$(n)_$(i)_target$(target)/"
             slice_bfs_rw(branch, slicer, reducer, dirname, 1)
 
             branch_df = CSV.read(joinpath(dirname, "slices.csv"), DataFrame)
             num_branches = length(branch_df.id)
             total_tc = log2(sum(2.0 .^ branch_df.tc))
-            @show i, ds, num_branches, total_tc
+            @show i, target, num_branches, total_tc
 
-            CSV.write(df, DataFrame(name = i, ds = ds, total_tc = total_tc, num_branch = num_branches), append = true)
+            CSV.write(df, DataFrame(name = i, target = target, total_tc = total_tc, num_branch = num_branches), append = true)
         end
     end
 
